6f2cb33fd4ec939c51243253703409b9e5ad4205
Manikanta9959 <sivachallano1@gmail.com>
2025-01-29 11:36:19 +0530

    matches tab content fix

--- frontend/src/reusable_components/MatchCard.js
+++ frontend/src/reusable_components/MatchCard.js
@@ -1,25 +1,25 @@
 import React from "react";
 
-const MatchCard = ({ teamA, teamB,teamAID, teamBID,areaID,areaName, league, date }) => {
+const MatchCard = ({ teamA, teamB, teamAID, teamBID, areaID, areaName, league, date }) => {
   return (
-    <button className="w-150 h-40 bg-white shadow-xl rounded-2xl flex flex-col justify-between items-start p-6 border border-gray-200 
+    <button className="w-150 h-50 bg-white shadow-xl rounded-2xl flex flex-col justify-start items-start p-6 border border-gray-200 
       transition-all duration-300 hover:bg-gray-200 hover:shadow-2xl active:scale-95">
-      <div>
+      <div className="w-full">
         <h2 className="text-xl font-bold text-gray-800">
           {teamA} <span className="text-red-500">vs</span> {teamB}
         </h2>
       </div>
-      <div>
+      <div className="w-full mt-2">
         <p className="text-gray-600 text-sm">
           <span className="font-semibold">League:</span> {league}
         </p>
       </div>
-      <div>
+      <div className="w-full mt-1">
         <p className="text-gray-600 text-sm">
           <span className="font-semibold">Area:</span> {areaName}
         </p>
       </div>
-      <div>
+      <div className="w-full mt-1">
         <p className="text-gray-600 text-sm">
           <span className="font-semibold">Date Scheduled:</span> {date}
         </p>
